373dfd7e1b54757a4b42306cff2c67daa12c12aa
dranawhite <1458755325@qq.com>
2018-06-27 09:54:32 +0800

    类名Refator

--- src/main/java/com/liumi/base/mybatis/plugin/DBgenerator.java
+++ src/main/java/com/liumi/base/mybatis/plugin/DBgenerator.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DBgenerrator {
+public class DBgenerator {
 
 	public static void autoDB(String configFilePath) {
 		List<String> warnings = new ArrayList<>();
